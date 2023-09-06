8c6a99a9667670a0cd7efb6b2bf89f80e111ec04
Hezrone Okoth <hezzyokoth@gmail.com>
2023-09-06 11:53:50 +0300

    Task 2: creates empty object if w or h is <= 0

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+        if (w > 0 || h > 0)
+    }
+    else {
+        Object.create()
+    }
+}
+module.exports = Rectangle
