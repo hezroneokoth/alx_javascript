28a452966f84af54f426803f6019977e2b256901
Hezrone Okoth <hezzyokoth@gmail.com>
2023-09-14 19:53:24 +0300

    Task 2: prints no of movies with Wedge Antilles

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+request.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    const filmsData = JSON.parse(body).results;
+    const WedgeAntillesCharacter = filmsData.filter(film =>
+        movies.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    );
+    console.log(WedgeAntillesCharacter.length);
+});
