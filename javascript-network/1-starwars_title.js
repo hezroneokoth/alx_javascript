51a919d542812569a378ac22b9b146ab2502d2e7
Hezrone Okoth <hezzyokoth@gmail.com>
2023-09-14 19:38:49 +0300

    Task 1 prints Star Wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -3,12 +3,11 @@
 const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-request.get(apiUrl, (error, response) => {
+request.get(apiUrl, (error, response, body) => {
     if (error) {
         console.error(error);
         return;
     }
-
     const movieData = JSON.parse(body);
     console.log(movieData.title);
 });
