292fa538cc9a5175ffdaef83af0ab8e289f16d39
Hezrone Okoth <hezzyokoth@gmail.com>
2023-09-14 22:21:33 +0300

    Task 3: Gets webpage contents & stores it in file

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -4,12 +4,13 @@ const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const Path = process.argv[3];
+
 request.get(url, (error, response, body) => {
     if (error) {
         console.error(error);
         return;
     }
-    fs.wFile(Path, body, 'utf-8', (wError) => {
+    fs.writeFile(Path, body, 'utf-8', (wError) => {
         if (wError) {
             console.error(wError);
             return;
