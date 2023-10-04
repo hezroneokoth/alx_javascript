004315caef16eb2f0611afd8205597e908aefd54
Hezrone Okoth <hezzyokoth@gmail.com>
2023-10-04 11:36:02 +0300

    Task 2: function w one arg a closure for an alert

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 function welcomeMessage(fullName) {
-    alert(`Welcome ${fullName}`);
+    return function() {
+        alert(`Welcome ${fullName}`);
+    };
     let guillaume = welcomeMessage('Guillaume');
     let alex = welcomeMessage('Alex');
     let fred = welcomeMessage('Fred');
