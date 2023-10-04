38a1f2ff3a4d8e7a856dee5821df5aa0b7bf456d
Hezrone Okoth <hezzyokoth@gmail.com>
2023-10-04 11:32:44 +0300

    Task 2: function that accepts one argument

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+function welcomeMessage(fullName) {
+    alert(`Welcome ${fullName}`);
+    let guillaume = welcomeMessage('Guillaume');
+    let alex = welcomeMessage('Alex');
+    let fred = welcomeMessage('Fred');
+}
