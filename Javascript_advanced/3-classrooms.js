9eb32ac2bc2e16be5dfecd6c96a511f9edf79f16
Hezrone Okoth <hezzyokoth@gmail.com>
2023-10-07 07:40:26 +0300

    Task 3: Function with closure and loops

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -1,10 +1,32 @@
 #!/usr/bin/node
 
-function welcomeMessage(fullName) {
-    return function() {
-        alert(`Welcome ${fullName}`);
-    };
-    let guillaume = welcomeMessage('Guillaume');
-    let alex = welcomeMessage('Alex');
-    let fred = welcomeMessage('Fred');
+/* the outer function takes the numberOfStudents as an arg,
+then the inner fucnction called studentSeat takes seat as an arg
+and returns a function that returns the seat no when called */
+function createClassRoom(numberOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            return seat;
+        };
+    }
+
+    let students = [];
+
+    /* This block creates a loop that iterates from 0
+    and pushes a new function created by the studentSeat function
+    into the students array after each iteration*/
+    for (let i = 0; i < numberOfStudents; i++) {
+        students.push(studentSeat(i + 1));
+    }
+
+    return students;
 }
+
+/* This block creates the closure with 10 students*/
+let classRoom = createClassRoom(10);
+
+/* This block calls the functions returned by studentSeat
+in the classRoom array so as to get the seat nos*/
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
